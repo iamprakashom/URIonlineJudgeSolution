00b78c6d78208abb4e47b54bb01a95437f62a96a
Om Prakash <iamprakashom@users.noreply.github.com>
2015-10-14 19:07:34 +0530

    1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a, b;
+    cin>>a;
+    cin>>b;
+    int sum;
+    sum = a + b;
+    cout<<"SOMA = "<<sum<<endl;
+    return 0;
+}
